586ccf7bbe9984eba9c85757a3a4f8b60a859cba
Diego Hurtado Vargas <diegohurtado2412@gmail.com>
2022-11-23 17:09:42 -0400

    deleting cosole logs

--- database/config.js
+++ database/config.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 
+require('dotenv').config();
+
 
 const dbConnection = async () => {
   try {
     console.log(process.env.DB_CNN);
     await mongoose.connect(process.env.DB_CNN);
+    console.log('DB Online');
+
   } catch (error) {
     throw new Error("Error en la base de datos - Hable con el admin.");
   }
