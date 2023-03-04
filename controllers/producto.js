db09cd73ad6173d3a9a8d74b8da19964934df4b9
Diego785 <diegohurtado2412@gmail.com>
2023-03-04 19:44:56 -0400

    joining changes

--- controllers/producto.js
+++ controllers/producto.js
@@ -2,6 +2,11 @@ const { ObjectId } = require('mongodb');
 const { response } = require("express");
 const Producto = require("../models/producto");
 const Categoria = require("../models/categoria");
+const Paciente = require("../models/paciente");
+const User = require("../models/usuario");
+const { populate } = require('../models/producto');
+const Lote = require('../models/lote');
+const { Model } = require('mongoose');
 
 const crearProducto = async (req, res = response) => {
   const producto = new Producto(req.body);
@@ -14,8 +19,32 @@ const crearProducto = async (req, res = response) => {
   });
 };
 
+const editProduct = async (req, res = response) => {
+  const id = req.params.id;
+  let producto = await Producto.findOneAndUpdate({ _id: id }, { nombre: req.body.nombre, descripcion: req.body.descripcion });
+
+  console.log(producto);
+
+
+  res.json({
+    ok: true,
+    body: req.body,
+  });
+};
+
 const getProduct = async (req, res = response) => {
-  const myProducts = await Producto.find().sort("fechaVencimiento");
+  // const patient = await Paciente.find().populate({ 
+  //     path: 'usuario', 
+  //     select: 'email', 
+  //     populate:{  
+  //       path: 'rol',
+  //       select: 'nombre'
+  //     } 
+  //   });
+  const myProducts = await Producto.find().populate({
+    path: 'categoria',
+    select: ['nombre']
+  });
 
   res.json({
     ok: true,
@@ -24,18 +53,41 @@ const getProduct = async (req, res = response) => {
 };
 
 const getEspecificProducts = async (req, res = response) => {
-  const d = new Date();
-  const d2 = new Date(d.getFullYear(), d.getMonth(), d.getDate());
-  const d3 = new Date(d.getFullYear(), d.getMonth(), d.getDate() + 7);
-  const myProducts = await Producto.find({"fechaVencimiento":{$gte: d2, $lte: d3}}).select("nombre , fechaVencimiento").limit(30);
-  res.json({
-    ok: true,
-    myProducts
-  });
+  // const d = new Date();
+  // const d2 = new Date(d.getFullYear(), d.getMonth(), d.getDate());
+  // const d3 = new Date(d.getFullYear(), d.getMonth(), d.getDate() + 7);
+  // const myProducts = await Producto.find({ "fechaVencimiento": { $gte: d2, $lte: d3 } }).select("nombre , fechaVencimiento").limit(30);
+  // res.json({
+  //   ok: true,
+  //   myProducts
+  // });
 };
 
 const getAvailableProducts = async (req, res = response) => {
-  const myProducts = await Producto.find().sort("fechaVencimiento").limit(2);
+
+  const d = new Date();
+  const d2 = new Date(d.getFullYear(), d.getMonth(), d.getDate());
+  const lotes = await Lote.find({ "fechaVencimiento": { $gte: d2 } }).populate({
+    path: 'producto', populate: {
+      path: 'categoria',
+      select: 'nombre'
+    }
+  });
+
+  let myProducts = [];
+  let b;
+  lotes.forEach(function (arrayItem) {
+    // console.log(arrayItem.producto);
+    b = true;
+    for (let index = 0; index < myProducts.length; index++) {
+      if (myProducts[index]._id == arrayItem.producto._id) {
+        b = false;
+      }
+    }
+    if (b == true)
+      myProducts.push(arrayItem.producto);
+  });
+
 
   res.json({
     ok: true,
@@ -44,7 +96,21 @@ const getAvailableProducts = async (req, res = response) => {
 };
 
 const getExpiratedProducts = async (req, res = response) => {
-  const myProducts = await Producto.find().sort("fechaVencimiento").limit(1);
+  const d = new Date();
+  const d2 = new Date(d.getFullYear(), d.getMonth(), d.getDate());
+  const lotes = await Lote.find({ "fechaVencimiento": { $lte: d2 } }).populate({
+    path: 'producto', populate: {
+      path: 'categoria',
+      select: 'nombre'
+    }
+  });
+
+  let myProducts = [];
+  lotes.forEach(function (arrayItem) {
+    // console.log(arrayItem.producto);
+    myProducts.push(arrayItem.producto);
+  });
+
 
   res.json({
     ok: true,
@@ -61,13 +127,31 @@ const getCategories = async (req, res = response) => {
   });
 };
 
-const getProductoforId = async(req,res) => {
+const getProductoforId = async (req, res) => {
   const productoId = req.params.codigo;
-  const producto = await Producto.findOne({_id : ObjectId(productoId)});
+  const producto = await Producto.findOne({ _id: ObjectId(productoId) });
   res.json({
-      ok: true,
-      producto: producto
+    ok: true,
+    producto: producto
   });
 }
 
-module.exports = { crearProducto, getProduct, getEspecificProducts, getAvailableProducts, getExpiratedProducts, getCategories, getProductoforId };
+const getProductsByCategory = async (req, res) => {
+  const categoryId = req.params.category;
+  const productos = await Producto.find({ categoria: categoryId }).populate({ path: 'categoria' });
+  res.json({
+    ok: false,
+    myProducts: productos
+  });
+}
+
+const getQuantityOfProductsByCategory = async (req, res) => {
+  const categoryId = req.params.category;
+  const productos = await Producto.find({ categoria: categoryId }).populate({ path: 'categoria' });
+  res.json({
+    ok: false,
+    cantidad: productos.length
+  });
+}
+
+module.exports = { crearProducto, editProduct, getProduct, getEspecificProducts, getAvailableProducts, getExpiratedProducts, getCategories, getProductoforId, getProductsByCategory, getQuantityOfProductsByCategory };
--- routes/productos.js
+++ routes/productos.js
@@ -6,14 +6,17 @@
 
 const { Router } = require("express");
 const router = Router();
-const { crearProducto, getProduct, getEspecificProducts, getAvailableProducts, getExpiratedProducts, getCategories, getProductoforId } = require("../controllers/producto");
+const { crearProducto, getProduct, getEspecificProducts, getAvailableProducts, getExpiratedProducts, getCategories, getProductoforId, getProductsByCategory, getQuantityOfProductsByCategory, editProduct } = require("../controllers/producto");
 
 router.post("/new", crearProducto);
+router.post("/edit/:id", editProduct);
 router.get("/", getProduct);
 router.get("/specific-products", getEspecificProducts );
 router.get("/available-products", getAvailableProducts );
 router.get("/expirated-products", getExpiratedProducts );
 router.get("/categories", getCategories );
 router.get("/unique-product/:codigo", getProductoforId  );
+router.get("/by-category/:category", getProductsByCategory  );
+router.get("/quantity-category/:category", getQuantityOfProductsByCategory  );
 
 module.exports = router;
